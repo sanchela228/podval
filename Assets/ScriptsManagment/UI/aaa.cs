66c0f6796c3b18f7efdac7f15c1c4a4ee91dc2ea
sanchela228 <94424298+sanchela228@users.noreply.github.com>
2022-02-10 00:46:09 +0500

    wer
    sanek

--- Assets/ScriptsManagment/UI/aaa.cs
+++ Assets/ScriptsManagment/UI/aaa.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dfghdfghfg : MonoBehaviour
+public class aaa : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
