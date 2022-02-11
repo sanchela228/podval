eff289a0c00a9a8d51a4d3f3f5e495dfc552e4ad
sanchela228 <94424298+sanchela228@users.noreply.github.com>
2022-02-11 22:09:59 +0500

    xml started

--- Assets/ScriptsManagment/local/DataBase/XML/XMLManager.cs
+++ Assets/ScriptsManagment/local/DataBase/XML/XMLManager.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace DataBase.XML
 {
+    [System.Serializable]
     public class XMLManager : Singleton
     {
-        // Наследуемся от синглтона естесно и нужно будет сделать 
-        // здесь работу с xml
+        // Singleton extension "of"course 
+        // xml working
+        public void test()
+        {
+            var instance = XMLManager.GetInstance();
+        }
 
-
+        
     }
 
 
