1cfa73169a77785b237bf853b1ac17de5687ee54
sanchela228 <94424298+sanchela228@users.noreply.github.com>
2022-03-14 01:27:04 +0500

    Items fix lits

--- Assets/ScriptsManagment/local/Models/ItemsDataBase.cs
+++ Assets/ScriptsManagment/local/Models/ItemsDataBase.cs
@@ -1,14 +1,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 namespace Models
 {
     [CreateAssetMenu(fileName = "New Item DataBase", menuName = "Assets/DataBases/Item DataBase")]
     public class ItemsDataBase : BaseScriptableObject
     {
-        public List<Item> listItem;
-    }
+        [SerializeField]
+        private List<Item> _listItem;
 
+        public List<Item> listItem => this._listItem;
+
+
+
+        //private static ItemsDataBase _instance;
+        //public static ItemsDataBase Instance
+        //{
+        //    get
+        //    {
+        //        if (!_instance)
+        //        {
+        //            _instance = Resources.<ItemsDataBase>();
+        //        }
+
+
+        //        return _instance;
+        //    }
+        //}
+
+
+        //private static ItemsDataBase instance;
+        //private ItemsDataBase() { }
+
+        //public static ItemsDataBase getInstance()
+        //{
+        //    if (instance == null)
+        //        instance = new ItemsDataBase();
+        //    return instance;
+        //}
+    }
 }
 
