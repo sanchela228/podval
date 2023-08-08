26a0abbc30fbfeb2957b0f9824deb47a8e9a9cea
vulf635 <60370094+vulf635@users.noreply.github.com>
2023-08-08 14:06:44 +0500

    suka
    123

--- Assets/ScriptsManagment/Scene/MainSceneManager.cs
+++ Assets/ScriptsManagment/Scene/MainSceneManager.cs
@@ -7,7 +7,8 @@ public class MainSceneManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("asdad");
+            
     }
 
     // Update is called once per frame
