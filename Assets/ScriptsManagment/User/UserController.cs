46e3ed6e2e43dcb31167dcd9c32b410b522de03c
sanchela228 <94424298+sanchela228@users.noreply.github.com>
2023-08-14 16:34:00 +0500

    Update 14.08 16:33
    create chest tests and micro fix

--- Assets/ScriptsManagment/User/UserController.cs
+++ Assets/ScriptsManagment/User/UserController.cs
@@ -1,5 +1,6 @@
 using Models;
 using Models.Environments;
+using Models.Items;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -25,16 +26,25 @@ public class UserController : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.K))
         {
             var ResourcesItem = Resources.Load<GameObject>("Prefabs/MapObject");
+            Camera camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+            var test = camera.ScreenToWorldPoint(Input.mousePosition);
+            test.z = 0;
 
             GameObject prefab = UnityEngine.Object.Instantiate<GameObject>(
                 ResourcesItem,
-                new Vector3(0, 0, -2),
+                test,
                 Quaternion.identity
             );
 
             Chest objectscript = UnityEngine.Object.Instantiate<Chest>(
                 Resources.Load<Chest>("ScriptableObject/Data/Environments/GayChest")
             );
+
+            Head objectHead = UnityEngine.Object.Instantiate<Head>(
+                Resources.Load<Head>("ScriptableObject/Data/Items/Head")
+            );
+
+            objectscript.Items.Add(objectHead);
 
             prefab.GetComponent<MapObject>().Environment = objectscript;
 
